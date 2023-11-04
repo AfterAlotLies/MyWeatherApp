976d507945663cf94315e46f4107ce519e5fbc08
Vyacheslav <guzxaroh@gmail.com>
2023-11-04 17:20:04 +0700

    Added string and image resources

---
+++ myWeatherApp/ImageResources.swift
@@ -0,0 +1,13 @@
+//
+//  ImageResources.swift
+//  myWeatherApp
+//
+//  Created by Vyacheslav on 04.11.2023.
+//
+
+import UIKit
+
+public enum ImageResources {
+    
+    static let mightBeCloudyAtNight = UIImage(named: "ПеременнаяОблачностьНочь")
+}
---
+++ myWeatherApp/LocalizedStrings.swift
@@ -0,0 +1,13 @@
+//
+//  LocalizedStrings.swift
+//  myWeatherApp
+//
+//  Created by Vyacheslav on 04.11.2023.
+//
+
+import Foundation
+
+public enum LocalizedStrings {
+    
+    static let weatherForecastNextSixDays = NSLocalizedString("Weather.Forecast.NextSixDays", comment: "")
+}

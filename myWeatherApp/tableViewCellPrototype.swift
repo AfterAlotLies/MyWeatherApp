bddcad87bf74099cd8fe70e7fe04e57552663531
Vyacheslav <guzxaroh@gmail.com>
2023-11-06 17:51:07 +0700

    create new class for cells in tableView

---
+++ myWeatherApp/tableViewCellPrototype.swift
@@ -0,0 +1,15 @@
+//
+//  tableViewCellPrototy[e.swift
+//  myWeatherApp
+//
+//  Created by Vyacheslav on 06.11.2023.
+//
+
+import UIKit
+
+class cellPrototype : UITableViewCell {
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var dayOfWeekLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+}

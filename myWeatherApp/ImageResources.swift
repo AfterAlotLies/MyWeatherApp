50665a70c83127803d0b5400bcd4f21fa3934137
Vyacheslav <guzxaroh@gmail.com>
2023-11-06 17:47:38 +0700

    added new Images

--- myWeatherApp/ImageResources.swift
+++ myWeatherApp/ImageResources.swift
@@ -8,6 +8,20 @@
 import UIKit
 
 public enum ImageResources {
-    
-    static let mightBeCloudyAtNight = UIImage(named: "ПеременнаяОблачностьНочь")
+
+    static let mightBeCloudyAtNight = UIImage(named: "mightBeCloudyAtNight")
+    static let littleSnow = UIImage(named: "littleSnow")
+    static let littleColdRain = UIImage(named: "littleColdRain")
+    static let  littleDark = UIImage(named: "littleDark")
+    static let cloudy = UIImage(named: "cloudy")
+    static let littleRainWithSnow = UIImage(named: "littleRainWithSnow")
+    static let sometimesSnow = UIImage(named: "sometimesSnow")
+    static let fog = UIImage(named: "fog")
+    static let coldFog = UIImage(named: "coldFog")
+    static let sunny = UIImage(named: "sunny")
+    static let clear = UIImage(named: "cloudy")
+    static let sometimesRain = UIImage(named: "sometimesRain")
+    static let strongSnow = UIImage(named: "strongSnow")
+    static let veryStrongSnow = UIImage(named: "veryStrongSnow")
+    static let averageSnow = UIImage(named: "averageSnow")
 }
